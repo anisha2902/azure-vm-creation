e05fcdbd3abfeca7f595d280d40f0b0c8297292a
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-05 14:54:48 +0530

    Update provider.tf

--- vm-folder/provider.tf
+++ vm-folder/provider.tf
@@ -1,14 +1,13 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "7.9.0"
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
     }
   }
 }
 
-provider "google" {
-  project = "centering-line-445111-e9"
-  region  = "us-east1"  
-  zone    = var.zone
+provider "azurerm" {
+  features {}
+  use_oidc = true
 }
