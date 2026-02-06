70720432a2c89dd82091dc5ff055d06bbad9b722
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-06 12:09:29 +0530

    Update main.tf

--- vm-folder/main.tf
+++ vm-folder/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
   name                = "${var.name}-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
+  size                = "Standard_B1s"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
