90f8ad503ae696aaecb365ba3b0bfa21fb52b95c
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-06 16:21:44 +0530

    Update variables.tf

--- vm-folder/variables.tf
+++ vm-folder/variables.tf
@@ -7,3 +7,8 @@ variable "location" {
   type    = string
   default = "eastus"
 }
+
+variable "ssh_public_key" {
+  description = "SSH public key to add to the admin user"
+  type        = string
+}
