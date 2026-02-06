593dc792f6a31b7d6dbffd4e3b940714998a63c9
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-02-06 11:32:12 +0530

    Update main.tf

--- vm-folder/main.tf
+++ vm-folder/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = "${locals.name}-rg"
-  location = "${locals.location}"
+  location = locals.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
